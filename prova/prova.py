328c50cfc1dfa36b332ea6a4fa1fa48fbccff62d
Giulia Rocha <134004818+giuliarocha04@users.noreply.github.com>
2023-10-10 15:35:25 -0300

    Add files via upload

---
+++ prova/prova.py
@@ -0,0 +1,23 @@
+class Loja:
+    def __init__(self, nome, end, cpnj)
+        self.nome = nome
+        self.end = end
+        self.cpnj = cpnj
+
+class Cliente:
+    def __init__(self, nome, end, cpf, nascimento, senha)
+        self.nome = nome
+        self.end = end
+        self.cpf = cpf
+        self.nascimento = nascimento
+        self.senha = senha
+
+class Produto:
+    def __init__(self, nome, desc, valor)
+        self.nome = nome
+        self.desc = desc
+        self.valor = valor
+
+class Admin:
+    def __init__(self) -> None:
+        pass
